2d6bb1b937a246358ab5535c3aa71d8245f25cb4
Memo Khoury <majd.khoury93@gmail.com>
2018-12-21 14:50:12 -0500

    Part 2 completed

--- src/smart_contracts/contracts/StarNotary.sol
+++ src/smart_contracts/contracts/StarNotary.sol
@@ -1,25 +1,38 @@
 pragma solidity ^0.4.23;
 
-import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+import './ERC721Token.sol';
 
-contract StarNotary is ERC721 { 
+contract StarNotary is ERC721Token { 
 
     struct Star { 
         string name; 
         string starStory; 
         string ra; 
-        string dec; 
+        string dec;
         string mag;
     }
 
-    mapping(uint256 => Star) public tokenIdToStarInfo; 
+    mapping(uint256 => Star) public tokenIdToStarInfo;
+    mapping(uint256 => Star) public starIdToStarInfo;
     mapping(uint256 => uint256) public starsForSale;
 
-    function createStar(string _name, uint256 _tokenId) public { 
-        Star memory newStar = Star(_name);
+    function createStar(string _name, string _starStory, string _ra, string _dec, string _mag, uint256 _tokenId) public {
+        // create new star 
+        Star memory newStar = Star(_name, _starStory, _ra, _dec, _mag);
+
+        // verify star uniqueness
+        uint256 _starId = keccak256(_ra, _dec, _mag);
+
+        require(starIdToStarInfo[_starId] != newStar, "starId not unique");
+
+        // map new star by starId
+        starIdToStarInfo[_starId] = newStar;
+
+        // map new star by tokenId
         tokenIdToStarInfo[_tokenId] = newStar;
 
-        _mint(msg.sender, _tokenId);
+        // mint new star
+        this.mint(msg.sender, _tokenId);
     }
 
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
@@ -33,6 +46,7 @@ contract StarNotary is ERC721 {
         
         uint256 starCost = starsForSale[_tokenId];
         address starOwner = this.ownerOf(_tokenId);
+
         require(msg.value >= starCost);
 
         _removeTokenFrom(starOwner, _tokenId);
